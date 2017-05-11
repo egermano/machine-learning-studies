4a175fa1f81fe070599411baeec7eb9330b34e0e
Bruno Germano <bruno.germano@gmail.com>
2017-05-11 21:05:20 +0000

    First commit

---
+++ hello-world.py
@@ -0,0 +1,17 @@
+from sklearn import tree
+# 1 = "smooth"
+# 0 = "Bumpy"
+features = [
+    [140, 1],
+    [130, 1],
+    [150, 0],
+    [170, 0]
+]
+# 0 = "apple"
+# 1 = "orange"
+labels = [0, 0, 1, 1]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+print (clf.predict([[160, 0]]))
