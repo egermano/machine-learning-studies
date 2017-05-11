e2835c377320318dc6b03585f003b58f86884c75
Bruno Germano <bruno.germano@gmail.com>
2017-05-11 21:54:11 +0000

    good features

--- dogs.py
+++ dogs.py
@@ -1,3 +1,6 @@
+# What Makes a Good Feature? - Machine Learning Recipes #3
+# https://www.youtube.com/watch?v=N9fDIAflCMY&index=3&list=PLOU2XLYxmsIIuiBfYad6rFYQU_jL2ryal
+
 import numpy as np
 import matplotlib.pyplot as plt
 
